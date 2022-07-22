ff1a8dee73b567dd5767d0a4d9a665611bd62453
zeptose <mcgrudy1232gmail.com>
2022-07-21 22:56:27 -0400

    xD

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <div className= "resume" id="resume">
       <div className="imgcontainer"> 
-        <img src="assets/resume1.png" alt="" />
+        <img src="assets/resume3.png" alt="" />
       </div>
     </div>
   )
